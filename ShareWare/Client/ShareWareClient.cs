94f7f7a066204cdfe9214d08fb372a1636049845
Amos Li <amos_402@msn.com>
2012-11-21 13:52:44 +0000

    git-tfs-id: [https://amos402.visualstudio.com]$/ShareWare;C17

---
+++ ShareWare/Client/ShareWareClient.cs
@@ -0,0 +1,62 @@
+﻿using ShareWare.ServiceReference;
+using ShareWare.ShareFile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareWare.Client
+{
+    public class ShareWareClient
+    {
+        private IShareService _client;
+        private int _userId;
+
+        public int UserId
+        {
+            get { return _userId; }
+            set { _userId = value; }
+        }
+
+        public ShareWareClient(IShareService client)
+        {
+            _client = client;
+        }
+
+        public bool Login(string userName, string passWord)
+        {
+            _userId = _client.Login(userName, passWord);
+
+            if (_userId != -1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void UploadFileInfo()
+        {
+            ShareFiles sh = new ShareFiles();
+            List<FileInfoTransfer> fileList = new List<FileInfoTransfer>();
+            foreach (var item in sh.ShareFileDict)
+            {
+                foreach (var item1 in item.Value)
+                {
+                    fileList.Add(new FileInfoTransfer());
+                }
+            }
+
+            _client.UploadShareInfoAsync(fileList, _userId);
+
+        }
+
+        public void ShearchFile(string fileName)
+        {
+            _client.SearchFile(fileName);
+        }
+
+
+    }
+}
