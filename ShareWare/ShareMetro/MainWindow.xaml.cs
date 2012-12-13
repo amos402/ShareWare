fb5b1c245c2771964fbd600468022a07e6aed019
Amos Li <amos_402@msn.com>
2012-12-13 03:28:54 +0000

    git-tfs-id: [https://amos402.visualstudio.com]$/ShareWare;C67

--- ShareWare/ShareMetro/MainWindow.xaml.cs
+++ ShareWare/ShareMetro/MainWindow.xaml.cs
@@ -7,11 +7,51 @@ using MahApps.Metro;
 using MahApps.Metro.Controls;
 using ShareWare.ShareFile;
 using Socket_Library;
+using System.Windows.Resources;
 
 namespace ShareMetro
 {
     public partial class MainWindow
     {
+        System.Windows.Forms.NotifyIcon notifyIcon;
+
+        public void IconShow()
+        {
+            StreamResourceInfo resourceInfo = Application.GetResourceStream(new Uri(@"images\sharewareIcon.ico", UriKind.Relative));
+
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon();
+            this.notifyIcon.BalloonTipText = "Hello, NotifyIcon!";
+            this.notifyIcon.Text = "Hello, NotifyIcon!";
+            this.notifyIcon.Icon = new System.Drawing.Icon(resourceInfo.Stream);
+            this.notifyIcon.Visible = true;
+            notifyIcon.MouseDoubleClick += notifyIcon_MouseDoubleClick;
+            // this.notifyIcon.ShowBalloonTip(500);
+        }
+
+        void notifyIcon_MouseDoubleClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+           WindowState = System.Windows.WindowState.Normal;
+           this.Visibility = System.Windows.Visibility.Visible;
+           // this.sho
+        }
+
+        void MainWindow_StateChanged(object sender, EventArgs e)
+        {
+            switch (WindowState)
+            {
+                case WindowState.Maximized:
+                    break;
+                case WindowState.Minimized:
+                    this.Visibility = System.Windows.Visibility.Hidden;
+                    break;
+                case WindowState.Normal:
+                    this.Visibility = System.Windows.Visibility.Visible;
+                    break;
+                default:
+                    break;
+            }
+        }
+
         public MainWindow()
         {
             DataContext = new MainWindowViewModel(Dispatcher);
@@ -20,16 +60,12 @@ namespace ShareMetro
             t.Tick += Tick;
             t.Interval = new TimeSpan(0, 0, 0, 1);
             t.Start();
-            var shit = new Flyout();
-            shit.Header = "fuck";
-            Flyouts.Add(shit);
-            this.Closing += MainWindow_Closing;
+            this.StateChanged += MainWindow_StateChanged;
+            this.Closing += ((sender1, e1) => notifyIcon.Visible = false);
+            IconShow();
         }
 
-        void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            
-        }
+
 
         void Tick(object sender, EventArgs e)
         {
@@ -39,8 +75,8 @@ namespace ShareMetro
         {
             // var x = pivot.Items;
 
-            Flyouts[0].IsOpen = !Flyouts[0].IsOpen;
-            Flyouts[2].IsOpen = !Flyouts[2].IsOpen;
+            //Flyouts[0].IsOpen = !Flyouts[0].IsOpen;
+            //Flyouts[2].IsOpen = !Flyouts[2].IsOpen;
 
         }
 
@@ -118,6 +154,7 @@ namespace ShareMetro
             this.IgnoreTaskbarOnMaximize = !this.IgnoreTaskbarOnMaximize;
         }
 
+
         #region ACE写的
         private void ListViewItem_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
@@ -125,7 +162,12 @@ namespace ShareMetro
             if (item.DataContext != null)
             {
                 FileInfoDataList model = item.SelectedItem as FileInfoDataList;
-                if (model.Type == "文件夹") (this.DataContext as MainWindowViewModel).LoadItems(model.Name);
+                if (model == null) return;
+                string name = "";
+                //if (model.Hash == null) 
+                name = model.Name;
+                //else name = model.Hash;
+                if (model.Type == "文件夹") (this.DataContext as MainWindowViewModel).LoadItems(name);
             }
         }
 
@@ -168,7 +210,7 @@ namespace ShareMetro
         private void Garbage_listview_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             SelectedItem(sender, e);
-        }      
+        }
 
         private void ListBox_Mouse_Double_Click(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
@@ -176,10 +218,35 @@ namespace ShareMetro
             if (item.DataContext != null)
             {
                 OnlineUserData model = item.SelectedItem as OnlineUserData;
+                if (model == null) return;
+                (this.DataContext as MainWindowViewModel).Directory = "";
                 (this.DataContext as MainWindowViewModel).CallLoad(model.UserName);
             }
         }
 
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.RoutedEvent.Name == "SelectionChanged")
+            {
+                ListBox item = sender as ListBox;
+                if (item.DataContext != null)
+                {
+                    OnlineUserData model = item.SelectedItem as OnlineUserData;
+                    (this.DataContext as MainWindowViewModel).SeleteManInfo = model;
+                }
+            }
+        }
+
+        private void Share_DoubleCilk(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ListView item = sender as ListView;
+            if (item.DataContext != null)
+            {
+                FileInfoDataList model = item.SelectedItem as FileInfoDataList;
+                if (model == null) return;
+                if (model.Type == "文件夹") (this.DataContext as MainWindowViewModel).Show_ShareFile(model.Name, (this.DataContext as MainWindowViewModel).ShareLoact);
+            }
+        }
         #endregion
 
     }
